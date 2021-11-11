1a905a94e53bf1a3923e586853946bba8a08518b
Andrii Podkolzin <apodkolzin@drbsystems.com>
2021-11-11 16:14:27 +0200

    Add sleep/delay benchmark

--- PreaparationToStream.Rehearsal/Program.cs
+++ PreaparationToStream.Rehearsal/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PreaparationToStream.Rehearsal
 {
+    public class SleepDelayBenchmark
+    {
+        [Benchmark(Baseline = true)]
+        public void Sleep() => Thread.Sleep(1);
+
+        [Benchmark]
+        public Task Delay() => Task.Delay(1);
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BenchmarkRunner.Run<SleepDelayBenchmark>(DefaultConfig.Instance.AddColumn(StatisticColumn.P95));
         }
     }
 }
