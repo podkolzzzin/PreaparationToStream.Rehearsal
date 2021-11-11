e12bdc511cd409bbd638fb6d8bd4793df04189b6
Andrii Podkolzin <apodkolzin@drbsystems.com>
2021-11-11 16:41:15 +0200

    Task Run/Thread Start/ThreadPool Queue Benchmark

--- PreaparationToStream.Rehearsal/Program.cs
+++ PreaparationToStream.Rehearsal/Program.cs
@@ -7,20 +7,66 @@ using System.Threading.Tasks;
 
 namespace PreaparationToStream.Rehearsal
 {
+    [MemoryDiagnoser]
     public class SleepDelayBenchmark
     {
+        [Params(1, 5, 50)]
+        public int DelayMS;
+
         [Benchmark(Baseline = true)]
-        public void Sleep() => Thread.Sleep(1);
+        public void Sleep() => Thread.Sleep(DelayMS);
 
         [Benchmark]
-        public Task Delay() => Task.Delay(1);
+        public Task Delay() => Task.Delay(DelayMS);
+    }
+
+    [MemoryDiagnoser]
+    public class TaskRunThreadStartPoolQueueBenchmark
+    {
+        [Benchmark]
+        public void TaskRun()
+        {
+            bool b = false;
+            Task.Run(() =>
+            {
+                b = true;
+            });
+            while (!Volatile.Read(ref b))
+                ;
+        }
+
+        [Benchmark]
+        public void ThreadPool()
+        {
+            bool b = false;
+            System.Threading.ThreadPool.QueueUserWorkItem(o =>
+            {
+                b = true;
+            });
+            while (!Volatile.Read(ref b))
+                ;
+        }
+
+        [Benchmark(Baseline = true)]
+        public void ThreadStart()
+        {
+            bool b = false;
+            var t = new Thread(() =>
+            {
+                b = true;
+            });
+            t.Start();
+            while (!Volatile.Read(ref b))
+                ;
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<SleepDelayBenchmark>(DefaultConfig.Instance.AddColumn(StatisticColumn.P95));
+            BenchmarkRunner.Run<TaskRunThreadStartPoolQueueBenchmark>();
+            //BenchmarkRunner.Run<SleepDelayBenchmark>(DefaultConfig.Instance.AddColumn(StatisticColumn.P95));
         }
     }
 }
