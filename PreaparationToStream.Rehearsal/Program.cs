d6895793b850fdd1b7b58b8f1ac2b68e8af121e2
Andrii Podkolzin <apodkolzin@drbsystems.com>
2021-11-11 16:07:35 +0200

    Add project files.

---
+++ PreaparationToStream.Rehearsal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PreaparationToStream.Rehearsal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
